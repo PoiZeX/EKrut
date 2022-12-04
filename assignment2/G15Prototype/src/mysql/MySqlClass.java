3330bab6e88ed4eed5cb41a7512fd22e3e0397ff
Dima <Dmitry.Chudnovsky@e.braude.ac.il>
2022-12-04 12:47:42 +0200

    Changed a typo

--- assignment2/G15Prototype/src/mysql/MySqlClass.java
+++ assignment2/G15Prototype/src/mysql/MySqlClass.java
@@ -17,7 +17,7 @@ public class MySqlClass {
 			connection = DriverManager.getConnection(DBAddress, username, password);
 			System.out.println("Connected To SQL");
 		} catch (SQLException ex) {
-			System.out.println("Expection: " + ex.getMessage());
+			System.out.println("Exception: " + ex.getMessage());
 			System.out.println("State: " + ex.getSQLState());
 			System.out.println("Error: " + ex.getErrorCode());
 			System.out.println("\n");
