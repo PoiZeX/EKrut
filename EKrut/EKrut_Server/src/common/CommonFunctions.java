c0acc4871c399ef7792e70de3732c6b8f895ff14
David Asulin <dudyas6@gmail.com>
2022-12-27 11:03:09 +0200

    removed generators from common funcs

--- EKrut/EKrut_Server/src/common/CommonFunctions.java
+++ EKrut/EKrut_Server/src/common/CommonFunctions.java
@@ -4,13 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import entity.OrderReportEntity;
 import javafx.application.Platform;
 import mysql.MySqlClass;
 
@@ -92,51 +89,6 @@ public class CommonFunctions {
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}
-		
-		
-	}
-
-	private static void generateOrdersReport(String month, String year) {
-		HashMap<String, String> regionDescription = new HashMap<>(); 
-		String query = "SELECT machines.machine_id, machines.machine_name, regions.region_name, orders.buytime, SUM(orders.total_sum) as total_sum, COUNT(*) as num_orders "
-				+ "FROM orders " + "JOIN machines ON orders.machine_id = machines.machine_id "
-				+ "JOIN regions ON machines.region_id = regions.region_id "
-				+ "WHERE orders.is_delivery = 0 AND STR_TO_DATE(buytime, '%d/%m/%Y %H:%i') BETWEEN ? AND ? "
-				+ "GROUP BY machines.machine_name";
-		try {
-			if (MySqlClass.getConnection() == null)
-				return;
-			Connection conn = MySqlClass.getConnection();
-			PreparedStatement psGet = conn.prepareStatement(query);
-			psGet.setString(1, String.format("%s-%s-01 00:00:00", year, month));
-			psGet.setString(2, String.format("%s-%s-31 23:59:59", year, month));
-			ResultSet res = psGet.executeQuery();
-			while (res.next()) {
-				if (regionDescription.get(res.getString(3)) == null) {
-					regionDescription.put(res.getString(3), "");
-				}
-				String description = regionDescription.get(res.getString(3));
-				String toAdd = String.format("%s,%s,%s", res.getString(2), res.getString(6), res.getString(5));
-				if (regionDescription.get(res.getString(3)).equals("")) {
-					description = toAdd;
-				} else {
-					description = description + "," + toAdd;
-				}
-				regionDescription.put(res.getString(3), description);
-			}
-			for (String reportKey : regionDescription.keySet()) {
-				PreparedStatement psInsert = conn.prepareStatement(
-				        "insert into orders_report(description,month,year,region) " +
-				        "values(?,?,?,?)");
-				psInsert.setString(1, regionDescription.get(reportKey));
-				psInsert.setString(2, month);
-				psInsert.setString(3, year);
-				psInsert.setString(4, reportKey);
-				psInsert.executeUpdate();
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		}	
 	}
 }
--- EKrut/EKrut_Server/src/common/ReportsGenerator.java
+++ EKrut/EKrut_Server/src/common/ReportsGenerator.java
@@ -141,5 +141,4 @@ public class ReportsGenerator {
 			e.printStackTrace();
 		}
 	}
-
 }
