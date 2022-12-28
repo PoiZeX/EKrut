2eb6a7cc037af28983f192362a9ff59ec0815eae
Vital Marciano <74133733+VitalMarciano@users.noreply.github.com>
2022-12-29 00:20:49 +0200

    confirm delivery/sels collect
    need to ganarate

---
+++ EKrut/EKrut_Client/src/controllerGui/ConfirmDeliveryController.java
@@ -0,0 +1,15 @@
+package controllerGui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class ConfirmDeliveryController {
+
+    @FXML
+    private Button submitBtn;
+
+    @FXML
+    private Label titleLbl;
+
+}
