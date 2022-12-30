fd07e713992e7296b96c621c6411cef333251473
Lidor <100760730+PoiZeX@users.noreply.github.com>
2022-12-30 22:57:40 +0200

    Update SQL (users) & users to import

--- EKrut/EKrut_Client/src/controllerGui/EKTPopupController.java
+++ EKrut/EKrut_Client/src/controllerGui/EKTPopupController.java
@@ -25,7 +25,11 @@ public class EKTPopupController extends LoginController {
 
 	protected static Timer timerSuccess;
 	protected static Timer timerTimeLimit;
+	
+	// define username and password to login with
+	private String[] usernamePasswordStub = new String[] { "customer4", "123456" }; // customer[1-3: registered, 4-6: member]
 
+	
 	/**
 	 * Initialize screen
 	 */
@@ -118,13 +122,4 @@ public class EKTPopupController extends LoginController {
 		s.setLoginBtnDisable(false);
 	}
 
-	private String[] usernamePasswordStub = new String[] { "subscribed", "123456" };
-
-//	/**
-//	 * Sends a waits for an answer if user valid or not
-//	 */
-//	protected void sendServerUsernamePassword(String[] usernamePassword) {
-//		chat.acceptObj(new Message(TaskType.RequestUserFromDB, usernamePassword));
-//	}
-//	
 }
