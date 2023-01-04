aed5159a908bf130165ab9d3d53b655f560a2262
Dima <Dmitry.Chudnovsky@e.braude.ac.il>
2023-01-04 12:21:53 +0200

    viewCatalogController

--- EKrut/EKrut_Client/src/controllerGui/ViewCatalogContoller.java
+++ EKrut/EKrut_Client/src/controllerGui/ViewCatalogContoller.java
@@ -1,199 +1,101 @@
 package controllerGui;
 
-import Store.NavigationStoreController;
-import common.ScreensNames;
+import java.util.ArrayList;
+import java.util.Timer;
+
+import common.Message;
+import common.TaskType;
+import entity.UserEntity;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import utils.TooltipSetter;
+import javafx.scene.layout.GridPane;
 
 public class ViewCatalogContoller {
 
-	private TooltipSetter tooltip;
+    @FXML
+    private Button AddToCartBtn;
 
-	@FXML
-	private Button cancelOrderBtn;
+    @FXML
+    private Button cancelOrderBtn;
 
-	@FXML
-	private Button placeOrderBtn;
+    @FXML
+    private GridPane catalogViewGridpane;
 
-	@FXML
-	private Button MinusQuantityBtn22;
+    @FXML
+    private Label discountPriceLabel;
 
-	@FXML
-	private TextField QuantityTxtField22;
+    @FXML
+    private Label discountPriceLabel1;
 
-	@FXML
-	private Button AddToCartBtn22;
+    @FXML
+    private Button minusBtn;
 
-	@FXML
-	private Label productNameLbl;
+    @FXML
+    private Button placeOrderBtn;
 
-	@FXML
-	private Label priceLbl;
+    @FXML
+    private Button plusBtn;
 
-	@FXML
-	private ImageView productItemImg22;
+    @FXML
+    private Label priceLabel;
 
-	@FXML
-	private Button AddQuantityBtn222;
+    @FXML
+    private Label priceLabel1;
 
-	@FXML
-	private Button MinusQuantityBtn222;
+    @FXML
+    private Label productLabel;
 
-	@FXML
-	private TextField QuantityTxtField222;
+    @FXML
+    private Label productLabel1;
 
-	@FXML
-	private Button AddToCartBtn222;
+    @FXML
+    private Label quntityLabel;
 
-	@FXML
-	private Label productNameLbl1;
+    @FXML
+    private Button searchBtn;
 
-	@FXML
-	private Label priceLbl1;
+    @FXML
+    private Button sortBtn;
 
-	@FXML
-	private ImageView productItemImg222;
-
-	@FXML
-	private Button AddQuantityBtn2221;
-
-	@FXML
-	private Button MinusQuantityBtn2221;
-
-	@FXML
-	private TextField QuantityTxtField2221;
-
-	@FXML
-	private Button AddToCartBtn2221;
-
-	@FXML
-	private Label productNameLbl11;
-
-	@FXML
-	private Label priceLbl11;
-
-	@FXML
-	private ImageView productItemImg2221;
-
-	@FXML
-	private Button AddQuantityBtn221;
-
-	@FXML
-	private Button MinusQuantityBtn221;
-
-	@FXML
-	private TextField QuantityTxtField221;
-
-	@FXML
-	private Button AddToCartBtn221;
-
-	@FXML
-	private ImageView productItemImg221;
-
-	@FXML
-	private Label productNameLbl11priceLbl11;
-
-	@FXML
-	private ImageView itemPriceLbl;
-
-	@FXML
-	private Button AddQuantityBtn22211;
-
-	@FXML
-	private Button MinusQuantityBtn22211;
-
-	@FXML
-	private TextField QuantityTxtField22211;
-
-	@FXML
-	private Button AddToCartBtn22211;
-
-	@FXML
-	private Label productNameLbl111;
-
-	@FXML
-	private Label priceLbl111;
-
-	@FXML
-	private ImageView productItemImg22211;
-
-	@FXML
-	private Button AddQuantityBtn2211;
-
-	@FXML
-	private Button MinusQuantityBtn2211;
-
-	@FXML
-	private TextField QuantityTxtField2211;
-
-	@FXML
-	private Button AddToCartBtn2211;
-
-	@FXML
-	private ImageView productItemImg2211;
-
-	@FXML
-	private Button AddQuantityBtn222111;
-
-	@FXML
-	private Button MinusQuantityBtn222111;
-
-	@FXML
-	private TextField QuantityTxtField222111;
-
-	@FXML
-	private Button AddToCartBtn222111;
-
-	@FXML
-	private Label productNameLbl1111;
-
-	@FXML
-	private Label priceLbl1111;
-
-	@FXML
-	private ImageView productItemImg222111;
-
-	@FXML
-	private Button viewCartBtn;
-
-	@FXML
-	private AnchorPane cartList;
-
-	@FXML
-	private Label totalPriceLabel;
-
-	public void ViewCatalogController() {
-		initialize();
+    private static ArrayList<ProductInCatalog> productList;
+    private class ProductInCatalog {
+    	private String productName, price, discountPrice;
+    	public ProductInCatalog(String productName, String price, String discountPrice) {
+    		this.discountPrice = discountPrice;
+    		this.price = price;
+    		this.productName = productName;
+    	}
+		public String getProductName() {
+			return productName;
+		}
+		public String getPrice() {
+			return price;
+		}
+		public String getDiscountPrice() {
+			return discountPrice;
+		}
+    }
+    
+    
+    public void initialize() {
+		productList = new ArrayList<>();
 	}
+    // THIS NEEDS TO BE MOVED // ***
+    public static void addProductToCatalog(String productName, String price, String discountPrice) {
+    	ViewCatalogContoller.ProductInCatalog newProduct = new ViewCatalogContoller().new ProductInCatalog(productName, price, discountPrice);
+    	
+    }
+    // ********************** // ***
+    @FXML
+    void cancelOrder(ActionEvent event) {
 
-	@FXML
-	void cancelOrder(ActionEvent event) {
+    }
 
-	}
+    @FXML
+    void placeOrder(ActionEvent event) {
 
-	@FXML
-	void placeOrder(ActionEvent event) {
-		NavigationStoreController.getInstance().setCurrentScreen(ScreensNames.ReviewOrder);
-
-	}
-
-	@FXML
-	void viewCart(ActionEvent event) {
-		NavigationStoreController.getInstance().setCurrentScreen(ScreensNames.ViewCart);
-	}
-
-	public void initialize() {
-
-		tooltip = new TooltipSetter("Cancel the order");
-		cancelOrderBtn.setTooltip(tooltip.getTooltip());
-		tooltip = new TooltipSetter("Place your order and continue checkout");
-		placeOrderBtn.setTooltip(tooltip.getTooltip());
-	}
+    }
 
 }
