0dfc3f45f579a519b7f99a8a80787a9ee8730b38
netaamz <netaamz@gmail.com>
2023-01-07 15:53:19 +0200

    Update SaleType.java

--- EKrut/EKrut_Client/src/common/SaleType.java
+++ EKrut/EKrut_Client/src/common/SaleType.java
@@ -2,13 +2,18 @@ package common;
 
 public enum SaleType {
 
-	onePlusOne("1+1"),
-	tenPercent("10%"),
-	twentyPercent("20%"),
-	thirtyPercent("30%");
+	onePlusOne("1+1",1.00),
+	tenPercent("10%",0.1),
+	twentyPercent("20%",0.2),
+	thirtyPercent("30%",0.3);
 	private final String name;
-	SaleType(String name) {
+	private final double precentage;
+	SaleType(String name,double d) {
 		this.name=name;
+		this.precentage=d;
+	}
+	public double getPrecentage() {
+		return precentage;
 	}
 	public String getName() {
 		return name;
