f0d8ef91c758d45f9f5abbb71d01c0cfd7b685f1
Dima <100364897+ChDimitry@users.noreply.github.com>
2023-01-20 16:55:08 +0200

    Update SupplyReportControllerTest.java

--- EKrut/EKrut_Client/JUnitTests/controllerGui/SupplyReportControllerTest.java
+++ EKrut/EKrut_Client/JUnitTests/controllerGui/SupplyReportControllerTest.java
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.when;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -34,12 +35,25 @@ class SupplyReportControllerTest {
 	private boolean compareReports(SupplyReportEntity compareReport, SupplyReportEntity toReport) {
 		ArrayList<Boolean> boolArray = new ArrayList<>();
 		boolArray.add(compareReport.getId() == toReport.getId());
-		boolArray.add(compareReport.getYear() == toReport.getYear());
-		boolArray.add(compareReport.getMonth() == toReport.getMonth());
+		boolArray.add(compareReport.getYear().equals(toReport.getYear()));
+		boolArray.add(compareReport.getMonth().equals(toReport.getMonth()));
 		boolArray.add(compareReport.getMachine_id() == toReport.getMachine_id());
-		for (String[] itemSet : compareReport.getReportsList())
-			boolArray.add(toReport.getReportsList().contains(itemSet));
+		boolArray.add(compareReport.getReportsList().size() == toReport.getReportsList().size());
 		if (boolArray.contains(false)) return false;
+		
+		int numberOfDataSets = compareReport.getReportsList().size();
+		int numberOfEqualDataSets = 0;
+		
+		// Compare item data sets
+		for (String[] compareDataSet : compareReport.getReportsList()) {
+			for (String[] toDataSet : compareReport.getReportsList()) {
+				if (new ArrayList<String>(Arrays.asList(compareDataSet)).equals(new ArrayList<String>(Arrays.asList(toDataSet)))) {
+					numberOfEqualDataSets++;
+				}	
+			}
+		}
+		
+		if (numberOfDataSets != numberOfEqualDataSets) return false;
 		return true;
 	}
 
@@ -53,10 +67,8 @@ class SupplyReportControllerTest {
 
 	@Test
 	void testSuccessfulSupplyReport() throws Exception {
-
 		SupplyReportController.setReport("2022", "12", "1");
 		SupplyReportEntity actualResult = supplyReportController.getSupplyReportFromDB(1);
-
 		SupplyReportEntity expectedResult = setExpectedResult(63, 1, 7,
 				"1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26",
 				"0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0",
