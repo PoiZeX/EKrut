51078bb408b53b4dcd84ba2bf3e9f4296bf23086
Dima <Dmitry.Chudnovsky@e.braude.ac.il>
2023-01-20 23:58:20 +0200

    Update SupplyReportControllerTest_Server.java

--- EKrut/EKrut_Client/JUnitTests/controllerGui/SupplyReportControllerTest_Server.java
+++ EKrut/EKrut_Client/JUnitTests/controllerGui/SupplyReportControllerTest_Server.java
@@ -1,5 +1,7 @@
 package controllerGui;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -22,21 +24,23 @@ class SupplyReportControllerTest_Server {
 		boolArray.add(compareReport.getRegion().equals(toReport.getRegion()));
 		boolArray.add(compareReport.getMachine_id() == toReport.getMachine_id());
 		boolArray.add(compareReport.getReportsList().size() == toReport.getReportsList().size());
-		if (boolArray.contains(false)) return false;
-		
+		if (boolArray.contains(false))
+			return false;
+
 		int numberOfDataSets = compareReport.getReportsList().size();
 		int numberOfEqualDataSets = 0;
-		
+
 		// Compare item data sets
 		for (String[] compareDataSet : compareReport.getReportsList()) {
 			for (String[] toDataSet : toReport.getReportsList()) {
-				if (new ArrayList<String>(Arrays.asList(compareDataSet)).equals(new ArrayList<String>(Arrays.asList(toDataSet)))) {
+				if (new ArrayList<String>(Arrays.asList(compareDataSet))
+						.equals(new ArrayList<String>(Arrays.asList(toDataSet)))) {
 					numberOfEqualDataSets++;
-				}	
+				}
 			}
 		}
-		
-		if (numberOfDataSets != numberOfEqualDataSets) return false;
+		if (numberOfDataSets != numberOfEqualDataSets)
+			return false;
 		return true;
 	}
 
@@ -49,6 +53,9 @@ class SupplyReportControllerTest_Server {
 	}
 
 	@Test
+	// Functionality: Supply Report request from the server returns an existing Supply Report
+	// Input: String "2022", String "12", String "1", int 1
+	// Result: SupplyReportEntity
 	void testSuccessfulSupplyReport() throws Exception {
 		SupplyReportController.setReport("2022", "12", "1");
 		SupplyReportEntity actualResult = supplyReportController.getSupplyReportFromDB(1);
@@ -56,13 +63,32 @@ class SupplyReportControllerTest_Server {
 				"1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26",
 				"0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0",
 				"10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10", "12", "2022", "1");
-
 		assertTrue(compareReports(actualResult, expectedResult));
-
 	}
 
+	@Test
+	// Functionality: Supply Report request from the server with invalid credentials 
+	// Input: String "2024", String "12", String "1", int 1
+	// Result: null
+	void testNullSupplyReport() throws Exception {
+		SupplyReportController.setReport("2024", "12", "1");
+		SupplyReportEntity actualResult = supplyReportController.getSupplyReportFromDB(1);
+		assertNull(actualResult.getReportsList());
+	}
+	
+	@Test
+	// Functionality: Supply Report request from the server with no credentials 
+	// Input: String "2024", String "12", String "1", int 1
+	// Result: null
+	void testInvalidSupplyReport() throws Exception {
+		SupplyReportController.setReport("", "", "");
+		SupplyReportEntity actualResult = supplyReportController.getSupplyReportFromDB(0);
+		assertNull(actualResult.getReportsList());
+	}
 	
 	
+	
+
 	private SupplyReportEntity setExpectedResult(int id, int machine_id, int min_stock, String item_id,
 			String times_under_min, String end_stock, String month, String year, String region) {
 		return new SupplyReportEntity(id, machine_id, min_stock, item_id, times_under_min, end_stock, month, year,
